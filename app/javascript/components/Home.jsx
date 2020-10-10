c828ea6b0ee04a068183ab51dc113c4681dcaf96
Melissa Chen <melissaistp@gmail.com>
2020-10-10 15:31:58 -0700

    removed .sprockets-manifest. have a little bit of homepage code

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,22 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Dream Connections</h1>
-        <p className="lead">
-          Please make sure to connect to the world wide dreamscape.
-        </p>
-        <hr className="my-4" />
-        <Link
-          to="/friends"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-            Add connections
-        </Link>
-      </div>
+  <div id="container_all">
+    <div id="messageBox">
+      <button></button>
     </div>
   </div>
+  // <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+  //   <div className="jumbotron jumbotron-fluid bg-transparent">
+  //     <div className="container secondary-color">
+  //       <h1 className="display-4">Dream Connections</h1>
+  //       <p className="lead">
+  //         Please make sure to connect to the world wide dreamscape.
+  //       </p>
+  //       <hr className="my-4" />
+  //       <Link
+  //         to="/friends"
+  //         className="btn btn-lg custom-button"
+  //         role="button"
+  //       >
+  //           Add connections
+  //       </Link>
+  //     </div>
+  //   </div>
+  // </div>
 );
