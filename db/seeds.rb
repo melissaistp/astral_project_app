1a82bf8e2afbb19b994127e7513cec72c913b4db
Melissa Chen <melissaistp@gmail.com>
2020-10-12 18:49:03 -0700

    images appear. seeded with one entry (self)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
-9.times do |i|
-    Friend.create(
-      alias: "Friend #{i + 1}",
-      sign: "Taurus",
-      likeability: "Close friend",
-      experience: "Flying dream",
-      message: "Hope you're having a good time",
-      instruction: "Make this person suffer",
-      image: "assets/ghost.png"
-    )
-  end
+Friend.create(
+  alias: "you",
+  sign: "Taurus",
+  likeability: "Close friend",
+  experience: "Flying dream",
+  message: "Hope you're having a good time",
+  instruction: "Make this person suffer",
+  image: "/ghost.png"
+)
