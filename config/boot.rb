822a589c2990b3f5824fd130ff1d11e67c3929da
Melissa Chen <melissachen@adobe.com>
2026-08-25 15:41:11 -0700

    creds updates

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'logger' # ActiveSupport::LoggerThreadSafeLevel needs Logger already defined; not guaranteed on Ruby 3.1+.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
